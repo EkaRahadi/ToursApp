99fa90f410d92427e0fd824af9bb5407b4242ba3
EkaRahadi <ekarahadi@gmail.com>
2020-09-26 01:15:22 +0700

    Securing http headers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 //Error Handling
 const AppError = require('./utils/appError');
@@ -12,11 +14,24 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//Middleware
-app.use(express.json());
+//Global Middleware
+//Set Security http headers
+app.use(helmet());
+//Logging for development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'too many requests from this IP, please try again in an hour.',
+});
+
+app.use('/api', limiter);
+
+//body parser
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
